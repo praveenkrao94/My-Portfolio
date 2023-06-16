70341e11c9c06b126c1ab54b56889fc84c7d0994
praveenkrao94 <praveenkr0812@gmail.com>
2023-06-16 18:19:24 +0530

    slide done

--- js/app.js
+++ js/app.js
@@ -1,25 +1,44 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 24,
+
+
+const swiper = new Swiper('.mySwiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    // spaceBetween: 24,
     speed: 1000,
+    loop: true,
+
+    // If we need pagination
     pagination: {
-        el: ".swiper-pagination",
         clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+
+        prevEl: '.swiper-button-next',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
     },
     breakpoints: {
         0: {
-            slidesPerView: 2,
-            spaceBetween: 20,
+            slidesPerView: 1,
+            // spaceBetween: 20,
         },
         768: {
-            slidesPerView: 3,
-            spaceBetween: 40,
+            slidesPerView: 1,
+            // spaceBetween: 40,
         },
         1024: {
-            slidesPerView: 3,
-            spaceBetween: 50,
+            slidesPerView: 2,
+            // spaceBetween: 50,
         },
     },
 });
+
+
 
 // --------------------------------------------------------------------------------------------------------------------------
 
